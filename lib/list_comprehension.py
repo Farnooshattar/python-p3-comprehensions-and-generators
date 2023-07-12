9f6ac1b534d7fd78c26acf47528a5e01031943ef
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-07-11 21:28:04 -0400

    updates list comprehensions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+num_list = [0, 1, 3, 5, 7, 8, 9]
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n%2 == 0 ]
+
+sentence_list = ["Hello", "I'm doing great", "Python is fun"]
 
 def make_exclamation(sentence_list):
-    pass
+     return [n+"!" for n in sentence_list ]
